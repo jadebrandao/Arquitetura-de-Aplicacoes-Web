395acae8ab2fa58b9a0110cfcc6c5c7f561783c3
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2024-06-14 19:22:11 -0300

    Delete Fipe_Api_Atividade directory

--- Fipe_Api_Atividade/src/main/java/com/example/Fipe/repository/FipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Fipe.repository;
-
-import com.example.Fipe.model.FipeEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FipeRepository extends MongoRepository<FipeEntity, String> {
-}
